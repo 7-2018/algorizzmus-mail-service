6ea1f8d29677af62191ff61881bdbc5c5068b72e
Marta <marta.m2000@yahoo.com>
2025-07-23 21:16:09 +0200

    Feature/AL-18: Add route for sending verification account email

---
+++ src/main/kotlin/com/vim/algorizzmusmailservice/application/request/EmailCodeRequest.kt
@@ -0,0 +1,6 @@
+package com.vim.algorizzmusmailservice.application.request
+
+data class EmailCodeRequest(
+    val email: String,
+    val code: String
+)
---
+++ src/main/kotlin/com/vim/algorizzmusmailservice/service/EmailService.kt
@@ -0,0 +1,41 @@
+package com.vim.algorizzmusmailservice.service
+
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+@Service
+class EmailService(private val mailSender: JavaMailSender) {
+    private fun sendEmail(
+        to: String,
+        subject: String,
+        text: String,
+    ) {
+        val message = SimpleMailMessage()
+        message.setTo(to)
+        message.subject = subject
+        message.text = text
+        mailSender.send(message)
+    }
+
+    fun sendVerificationEmail(
+        to: String,
+        code: String,
+    ) {
+        val body = "Your verification code is $code"
+        sendEmail(to, REGISTRATION_EMAIL_SUBJECT, body)
+    }
+
+    fun sendPasswordResetEmail(
+        to: String,
+        code: String,
+    ) {
+        val body = "Your password reset code is $code"
+        sendEmail(to, PASSWORD_RESET_EMAIL_SUBJECT, body)
+    }
+
+    companion object {
+        private const val REGISTRATION_EMAIL_SUBJECT = "Verify your email"
+        private const val PASSWORD_RESET_EMAIL_SUBJECT = "Reset your password"
+    }
+}
