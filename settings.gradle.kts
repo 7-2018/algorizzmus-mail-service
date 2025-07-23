2b67b7b9b6af524ce55df28131d09e61b4a6047d
Marta <marta.m2000@yahoo.com>
2025-07-23 19:22:36 +0200

    Feature/AL-18: Initialize mail service

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "algorizzmus-mail-service"
---
+++ src/test/kotlin/com/vim/algorizzmusmailservice/AlgorizzmusMailServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vim.algorizzmusmailservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlgorizzmusMailServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
